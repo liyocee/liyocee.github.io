70190ccd7e30a5be5f5e2ae59a1718f8a1446fad
liyocee <collinskivale@gmail.com>
2016-08-05 20:22:46 +0300

    fix fab file

--- source/fabfile.py
+++ source/fabfile.py
@@ -11,15 +11,6 @@ from pelican.server import ComplexHTTPRequestHandler
 env.deploy_path = 'output'
 DEPLOY_PATH = env.deploy_path
 
-# Remote server configuration
-production = 'root@localhost:22'
-dest_path = '/var/www'
-
-# Rackspace Cloud Files configuration settings
-env.cloudfiles_username = 'my_rackspace_username'
-env.cloudfiles_api_key = 'my_rackspace_api_key'
-env.cloudfiles_container = 'my_cloudfiles_container'
-
 # Github Pages configuration
 env.github_pages_branch = "master"
 
@@ -74,33 +65,8 @@ def preview():
     local('pelican -s publishconf.py')
 
 
-def prod_build():
-    """Build production version of site"""
-    clean()
-    local('pelican -s publishconf.py')
-
-
-def cf_upload():
-    """Publish to Rackspace Cloud Files"""
-    rebuild()
-    with lcd(DEPLOY_PATH):
-        local('swift -v -A https://auth.api.rackspacecloud.com/v1.0 '
-              '-U {cloudfiles_username} '
-              '-K {cloudfiles_api_key} '
-              'upload -c {cloudfiles_container} .'.format(**env))
-
-
-@hosts(production)
 def publish():
-    """Publish to production via rsync"""
-    local('pelican -s publishconf.py')
-    project.rsync_project(
-        remote_dir=dest_path,
-        exclude=".DS_Store",
-        local_dir=DEPLOY_PATH.rstrip('/') + '/',
-        delete=True,
-        extra_opts='-c',
-    )
+    gh_pages()
 
 
 def gh_pages():
