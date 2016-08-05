163b2057cd39848a67632879e8f9bd5b3a699247
liyocee <collinskivale@gmail.com>
2016-08-05 20:06:20 +0300

    instaled ghp-import for deploying

--- source/publishconf.py
+++ source/publishconf.py
@@ -15,7 +15,6 @@ RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
-OUTPUT_PATH = '../../liyocee.github.io'
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
